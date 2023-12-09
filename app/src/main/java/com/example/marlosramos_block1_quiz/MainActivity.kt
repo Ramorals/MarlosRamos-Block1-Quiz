e3b80b2e381c7b4bc7fdb8548bf19f4c6d4ec99d
Marlos Ramos <arsa.ramos.up@phinmaed.com>
2023-12-09 12:27:28 +0800

    third commit

--- app/src/main/java/com/example/marlosramos_block1_quiz/MainActivity.kt
+++ app/src/main/java/com/example/marlosramos_block1_quiz/MainActivity.kt
@@ -1,11 +1,33 @@
-package com.example.marlosramos_block1_quiz
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextNumber1: EditText
+    private lateinit var editTextNumber2: EditText
+    private lateinit var radioGroupOperators: RadioGroup
+    private lateinit var buttonCalculate: Button
+    private lateinit var textViewResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextNumber1 = findViewById(R.id.editTextNumber1)
+        editTextNumber2 = findViewById(R.id.editTextNumber2)
+        radioGroupOperators = findViewById(R.id.radioGroupOperators)
+        buttonCalculate = findViewById(R.id.buttonCalculate)
+        textViewResult = findViewById(R.id.textViewResult)
+
+        buttonCalculate.setOnClickListener {
+            calculateResult()
+        }
+
     }
 }
